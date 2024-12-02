5d45ea88f074103413d1d1827608b30c889f214e
= <=>
2024-12-02 09:19:43 -0700

    updated App-link style and text

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactttttt
+          This is a test of the automated Github / Vercel Deployments. The text here was set on 12/2 at 9:17 and should be crimson, underlined, and linked to google.com
         </a>
       </header>
     </div>
